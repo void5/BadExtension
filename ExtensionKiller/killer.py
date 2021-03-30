c1fc166200dfbdfee0c24e3a82c2a13199cbcc18
Finnegan <mccarthy.finnegan@gmail.com>
2021-03-30 11:25:45 -0700

    Updated setup.py and added toml file for build process.
    Final basic edition to be released on PyPi shortly.

--- ExtensionKiller/killer.py
+++ ExtensionKiller/killer.py
@@ -2,6 +2,7 @@ import click
 import os
 import shutil
 
+
 @click.command()
 @click.option("--id", help="put the extension id here")
 def kill(id):
--- ExtensionKiller/setup.py
+++ ExtensionKiller/setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='ExtensionKiller',
     version='0.1',
+    author="Finnegan McCarthy",
+    author_email="mccarthy.finnegan@gmail.com",
+    description="A small script to kill a chrome extension that you specify the ID for, but it only works on Windows",
+    url="https://github.com/void5/BadExtension",
     install_requires=[
         'Click',
     ],
@@ -10,4 +14,5 @@ setup(
         [console_scripts]
         killer=killer:cli
     ''',
+    python_requires=">=3.7"
 )
